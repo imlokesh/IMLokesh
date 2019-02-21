3a253ec15dcb67975f93e5c00c0ba7c8933f4c28
imlokesh <me@imlokesh.com>
2019-02-21 14:28:09 +0530

    fix tests

--- IMLokesh.Tests/RandomTests.cs
+++ IMLokesh.Tests/RandomTests.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using IMLokesh.Random;
 using Xunit;
 
@@ -6,15 +9,21 @@ namespace IMLokesh.Tests
 {
     public class RandomTests
     {
-        [Theory]
-        [InlineData(10)]
-        [InlineData(20)]
-        [InlineData(50)]
-        public void RandomLowerCaseString(int length)
+        [Fact]
+        public void RandomLowerCaseString()
         {
+            var length = RandomIntWithMaxValue();
             var str = RandomHelper.RandomString(length, true);
             Assert.True(str.Length == length);
             Assert.True(str.ToLowerInvariant() == str);
+        }
+
+        [Fact]
+        public int RandomIntWithMaxValue()
+        {
+            var i = RandomHelper.RandomInt(100);
+            Assert.True(i >= 0 && i < 100);
+            return i;
         }
     }
 }
